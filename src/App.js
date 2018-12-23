b9d5cdd7b49881c632a7f3548d86ba80eb818a29
Zita Balint <zitag4@gmail.com>
2018-12-23 21:13:06 +0100

    add markers

--- src/App.js
+++ src/App.js
@@ -3,58 +3,74 @@ import './App.css';
 
 
 class App extends Component {
-  /*From npm: https://www.npmjs.com/package/google-map-react*/
-  static defaultProps = {
-    center: {
-      lat: 40.7413549,
-      lng: -73.9980244
-    },
-    zoom: 13
-  };
-
+  state = {
+    markers: []
+  /*    locations: [
+        {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
+        {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
+        {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
+        {title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
+        {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
+        {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
+        ]*/
+      }
   componentDidMount() {
-    this.renderMap()
-  }
-
-  renderMap = () => {
     loadScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyDRxmJRw4I4YQIMPSBFVuYfuWl79PLyDZQ&v=3&callback=initMap')
     window.initMap = this.initMap
   }
 
-   initMap = () => {
-        const  map = new window.google.maps.Map(document.getElementById('map'), {
-          center: {lat: 40.7413549, lng: -73.9980244},
-          zoom: 13
-          });
-        }
-/*  state = {
-    marks: [
+  initMap = () => {
+    // Create a new blank array for all the listing markers.
+    let markers = [];
+    const google = window.google;
+    const  map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 40.7413549, lng: -73.9980244},
+      zoom: 13
+    });
+
+    var locations = [
       {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
       {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
       {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
       {title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
       {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
       {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
-    ]} */
+    ];
 
+    // The following group uses the location array to create an array of markers on initialize.
+    for (var i = 0; i < locations.length; i++) {
+      // Get the position from the location array.
+      let position = locations[i].location;
+      let title = locations[i].title;
 
+      // Create a marker per location, and put into markers array.
+      let marker = new google.maps.Marker({
+        map: map,
+        position: position,
+        title: title,
+        animation: google.maps.Animation.DROP,
+        id: i
+      });
 
-    render() {
+      // Push the marker to our array of markers.
+      markers.push(marker);
+    }
+  }
 
-      return (
-        <div id='map'></div>
+  render() {
+    return (
+      <div id='map'></div>
     );
   }
 }
 
 function loadScript(src) {
-    let tag = window.document.getElementsByTagName('script')[0];
-    let script = window.document.createElement('script');
+  let tag = window.document.getElementsByTagName('script')[0];
+  let script = window.document.createElement('script');
 
-    script.src = src;
-    script.async = true;
-
-    tag.parentNode.insertBefore(script, tag);
+  script.src = src;
+  script.async = true;
+  tag.parentNode.insertBefore(script, tag);
 }
 
 export default App;
